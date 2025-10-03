5630684b67ea7ff12587f5b0109b94b1f4ddc607
samhaswon <sahoward42@tntech.edu>
2025-10-03 09:26:49 -0500

    Make example usage clearer

--- u2net/u2net_attempt.py
+++ u2net/u2net_attempt.py
@@ -3,6 +3,8 @@ from PIL import Image
 
 
 if __name__ == '__main__':
-    session = Session(model_path="D:\\skin_models\\5796.onnx")  # Replace with the path to the checkpoint
+    # Replace with the path to the checkpoint you want to use,
+    # u2net, u2netp, or dlmv
+    session = Session(model_path="/path/to/u2net.onnx")
     image_in = Image.open("images/000k9gtp50b3kj06bikpg230de34c.jpg")   # Replace with the path to your image
     session.remove(image_in).save("result.png")
